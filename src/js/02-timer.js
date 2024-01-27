560fa0de7ada2506e7c52f5e622929ce97ca541f
czke33 <czke33@gmail.com>
2024-01-27 11:36:07 +0200

    import elements and flatapicker

---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const elements = {
+  startBtn: document.querySelector('[data-start'),
+  dateInput: document.getElementById('datetime-picker'),
+  days: document.querySelector('[data-days'),
+  hours: document.querySelector('[data-hours'),
+  minutes: document.querySelector('[data-minutes'),
+  seconds: document.querySelector('[data-seconds'),
+};
