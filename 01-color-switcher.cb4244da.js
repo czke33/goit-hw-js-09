24ec38257b885a4edba6ef60c12418e86fa6831f
czke33 <143416739+czke33@users.noreply.github.com>
2024-01-27 09:29:47 +0000

    Deploying to gh-pages from @ czke33/goit-hw-js-09@b772a875d13365d6d067d23ac07d1c0b3f6dccbb 🚀

---
+++ 01-color-switcher.cb4244da.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.setAttribute("disabled","");let d=null;t.addEventListener("click",(()=>{t.setAttribute("disabled",""),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),e.setAttribute("disabled",""),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.cb4244da.js.map
