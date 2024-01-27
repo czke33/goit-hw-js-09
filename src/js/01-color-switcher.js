b772a875d13365d6d067d23ac07d1c0b3f6dccbb
czke33 <czke33@gmail.com>
2024-01-27 11:29:23 +0200

    add color changer 01 js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+stopBtn.setAttribute('disabled', '');
+
+let timer = null;
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+  clearInterval(timer);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
