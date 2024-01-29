8dbefda96cc0ef883bfd48a944ea37738db7a437
czke33 <czke33@gmail.com>
2024-01-29 08:21:26 +0200

    hw 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+const formElement = document.querySelector('.form');
+const buttonElement = document.querySelector('[type="submit"]');
+
+import Notiflix from 'notiflix';
+
+formElement.addEventListener('submit', onSubmitClick);
+
+function onSubmitClick(evt) {
+  evt.preventDefault();
+
+  let delay = Number(formElement.delay.value);
+  let step = Number(formElement.step.value);
+  let amount = Number(formElement.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
+  }
+  buttonElement.disabled = true;
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
